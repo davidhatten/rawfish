f5d47c2aa4ea5269b1fe38c5c195233ff4a0ecf9
David Hatten <david.r.hatten@gmail.com>
2015-03-21 15:35:09 -0400

    initial navigation

--- packages/custom/rawfish/public/routes/rawfish.js
+++ packages/custom/rawfish/public/routes/rawfish.js
@@ -2,9 +2,14 @@
 
 angular.module('mean.rawfish').config(['$stateProvider',
   function($stateProvider) {
-    $stateProvider.state('rawfish example page', {
-      url: '/rawfish/example',
-      templateUrl: 'rawfish/views/index.html'
-    });
+    $stateProvider
+    	.state('rawfish example page', {
+	      url: '/rawfish/example',
+	      templateUrl: 'rawfish/views/index.html'
+	    })
+    	.state('join game', {
+    		url:'/rawfish/join',
+    		templateUrl: 'rawfish/views/join.html'
+    	});
   }
 ]);
