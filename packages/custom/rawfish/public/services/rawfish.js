2d607593bd294fd7bb279d5a81a7e47d29edf7dd
Tim Garvin <tcgarvin@us.ibm.com>
2015-03-21 17:41:45 -0400

    Added sockets to service.  Probably working?

--- packages/custom/rawfish/public/services/rawfish.js
+++ packages/custom/rawfish/public/services/rawfish.js
@@ -1,9 +1,24 @@
 'use strict';
 
 angular.module('mean.rawfish').factory('Rawfish', [
-  function() {
-    return {
-      name: 'rawfish'
-    };
-  }
+    function() {
+        var socket = io.connect("/");
+        return {
+            name: 'rawfish',
+            on: function(eventName, callback) {
+                socket.on(eventName, function() {
+                    var args = arguments;
+                    $rootScope.$apply(function() {
+                        callback.apply(socket, args);
+                    });
+                });
+            },
+            emit: function(eventName, data, callback) {
+                socket.emit(eventName, data, function() {
+                    console.log('event:', eventName);
+                    var args = arguments;
+                });
+            }
+        };
+    }
 ]);
